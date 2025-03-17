ab00021d50039b8e15a374b545ecefbd5066b2aa
Feshal <fesalnaguji@gmail.com>
2025-03-17 08:26:14 +0530

    removed routes from vercel.json

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@
 require("dotenv").config(); // Loads .env into process.env
 
 module.exports = {
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 3000,
   mongoUri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/my-saas-db",
   env: process.env.NODE_ENV || "development",
   // Add more environment variables as needed
